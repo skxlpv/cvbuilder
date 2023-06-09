0626ca0c457e4865d9b35fb89d0682e644f45dde
skxlpv <olexandertym0000@gmail.com>
2023-06-09 12:59:22 +0300

    PDFkit html to pdf generation

--- app/admin/workers_admin.rb
+++ app/admin/workers_admin.rb
@@ -1,18 +1,3 @@
-class PdfController < ApplicationController
-  def generate_pdf
-    pdf = Prawn::Document.new
-
-    pdf.text 'This is a Heading', size: 18, style: :bold, align: :center, margin_bottom: 10
-    pdf.text 'This is a paragraph of text.', size: 12, align: :justify, margin_bottom: 20
-
-    pdf.font('Helvetica') do
-      pdf.text 'This is some text with a custom font.'
-    end
-
-    pdf.render_file('output.pdf')
-  end
-end
-
 Trestle.resource(:workers) do
   menu do
     item :workers, icon: "fa fa-star"
@@ -70,9 +55,25 @@ Trestle.resource(:workers) do
     end
     
     def generate_pdf      
-      PdfController.new.generate_pdf
+      options = {
+        page_size: 'A4',
+        disable_smart_shrinking: true,
+      }
+
+      worker = Worker.find(params[:id])
+
+      html_template = File.read('/home/skxlpv/OWN/cvbuilder/app/views/shared/cv_template.html.erb')
+      html = html_template.gsub('{worker_name}', worker.user.first_name)
+      
+      pdfkit = PDFKit.new(html, options)
+      pdf_data = pdfkit.to_pdf
+      
+      File.open("/home/skxlpv/OWN/cvbuilder/public/pdfs/cv_#{worker.user.first_name}.pdf", 'wb') do |file|
+        file << pdf_data
+      end
+      
+      send_data pdf_data, filename: 'pdf.pdf', type: 'application/pdf', disposition: 'inline'
     end
-    
   end
 
   routes do
